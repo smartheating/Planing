f7c06b636213776a891f6498bf3462d848f1a67e
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-05-23 13:37:42 +0200

    Init commit

---
+++ src/main/java/de/smartheating/planing/PlaningApplication.java
@@ -0,0 +1,13 @@
+package de.smartheating.planing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaningApplication.class, args);
+	}
+
+}
