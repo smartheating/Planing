4ba66421f3ea2ffd42b8f7aaf45a6242a7a5cb5d
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-05-23 17:17:46 +0200

    Added RabbitMQ support

--- src/main/java/de/smartheating/planing/rest/PlaningController.java
+++ src/main/java/de/smartheating/planing/rest/PlaningController.java
@@ -1,15 +1,21 @@
 package de.smartheating.planing.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.smartheating.SmartHeatingCommons.TestModel;
+import de.smartheating.planing.rabbitmq.MessageProducer;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class PlaningController {
+	
+	@Autowired
+	MessageProducer rabbitMQProducer;
+	
 	/**
 	 * This method returns a greeting.
 	 * 
@@ -20,6 +26,7 @@ public class PlaningController {
 	public ResponseEntity<?> testMessaging() {
 		TestModel test = new TestModel();
 		test.setTest("Repository is up!");
+		rabbitMQProducer.sendMessages("Test RabbitMQ");
 		return new ResponseEntity<>(test, HttpStatus.OK);
 	}
 }
