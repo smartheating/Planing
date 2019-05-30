982b768b482e411f026f371186110021c20c3737
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-05-30 08:43:33 +0200

    Cleaned up Database exclusion

--- src/main/java/de/smartheating/planing/PlaningApplication.java
+++ src/main/java/de/smartheating/planing/PlaningApplication.java
@@ -2,8 +2,12 @@ package de.smartheating.planing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+@EnableDiscoveryClient
 public class PlaningApplication {
 
 	public static void main(String[] args) {
